45258a21ff1d3ea0a3dfb55ecc7ef42cac403466
Aashay <arkhamknight.aashay@gmail.com>
2022-05-02 16:33:07 +0530

    file system basics

---
+++ node-practice/file-fs.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+
+
+// writing a file 
+// fs.writeFile('newfile.js', 'console.log("new file data")', function (err) {
+//     console.log('file saved!')
+// })
+
+
+// fs.appendFile - used to append , same as writeFile
+
+// // reading file
+// fs.readFile('./calc.js', 'utf8', function (err, data) {
+//     console.log(data)
+// })
+
+// // deleting file
+// fs.unlink('newfile.js', function (err) {
+//     console.log('file deleted !')
+// })
