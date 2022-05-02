b089b522bf1c294db6c921cc44d16a1eeec7712f
Aashay <arkhamknight.aashay@gmail.com>
2022-05-02 13:25:31 +0530

    module exports example

--- node-practice/app.js
+++ node-practice/app.js
@@ -2,5 +2,5 @@ const http = require('http')
 
 http.createServer((req,res)=>{
     res.writeHead(200,{'Content-Type':'text/html'})
-    res.end('hello')
+    res.end('hello , welcome to node js')
 }).listen(3001)
---
+++ node-practice/sample.js
@@ -0,0 +1,7 @@
+const calculate = require('./calc.js')
+
+const output = calculate.add(15,5)
+const output2 = calculate.sub(15,5)
+
+console.log('output is :',output)
+console.log('output2 is :',output2)
