5e8ec94d2bc3d2c3b9ea6f6bba741fb75357f99b
Aashay <arkhamknight.aashay@gmail.com>
2022-06-02 23:28:56 +0530

    added LL removeFrom Location

--- datastructures/LinkedList.js
+++ datastructures/LinkedList.js
@@ -48,11 +48,76 @@ class LinkedList {
 
 
     // insertAt(element, location)
-    // working here
+    // insert element at the position index
+    // of the list
+    insertAt(element, index) {
+        if (index < 0 || index > this.size)
+            return console.log("Please enter a valid index.");
+        else {
+            // creates a new node
+            var node = new Node(element);
+            var curr, prev;
 
+            curr = this.head;
 
+            // add the element to the
+            // first index
+            if (index == 0) {
+                node.next = this.head;
+                this.head = node;
+            } else {
+                curr = this.head;
+                var it = 0;
+
+                // iterate over the list to find
+                // the position to insert
+                while (it < index) {
+                    it++;
+                    prev = curr;
+                    curr = curr.next;
+                }
+
+                // adding an element
+                node.next = curr;
+                prev.next = node;
+            }
+            this.size++;
+        }
+    }
 
     // removeFrom(location)
+    // removes an element from the
+    // specified location
+    removeFrom(index) {
+        if (index < 0 || index >= this.size)
+            return console.log("Please Enter a valid index");
+        else {
+            var curr, prev, it = 0;
+            curr = this.head;
+            prev = curr;
+
+            // deleting first element
+            if (index === 0) {
+                this.head = curr.next;
+            } else {
+                // iterate over the list to the
+                // position to removce an element
+                while (it < index) {
+                    it++;
+                    prev = curr;
+                    curr = curr.next;
+                }
+
+                // remove the element
+                prev.next = curr.next;
+            }
+            this.size--;
+
+            // return the remove element
+            return curr.element;
+        }
+    }
+
     // removeElement(element)
 
     // Helper Methods
