7746faaef3884d96cd49472117b3bc8998788a6b
Aashay <arkhamknight.aashay@gmail.com>
2022-06-09 22:02:47 +0530

    added expanding object utility

--- basics/delete/delete-empty-array.js
+++ basics/delete/delete-empty-array.js
@@ -1,40 +1,21 @@
 // delete the mealType which has an empty array of items
+const util = require('util')
+
 const order = [
-    {
-        "2022/06/08": {
-            "Breakfast": {
-                "items": [
-                    "item1",
-                    "item2",
-                    "item3"
-                ]
-            },
-            "Lunch": {
-                "items": []
-            },
-            "Dinner": {
-                "items": [
-                    "item1",
-                    "item2",
-                    "item3"
-                ]
-            }
-        }
-    },
     {
         "2022/06/09": {
             "Breakfast": {
                 "items": []
             },
             "Lunch": {
-                "items": []
-            },
-            "Dinner": {
                 "items": [
                     "item1",
                     "item2",
                     "item3"
                 ]
+            },
+            "Dinner": {
+                "items": []
             }
         }
     },
@@ -44,10 +25,31 @@ const order = [
                 "items": []
             },
             "Lunch": {
-                "items": []
+                "items": [
+                    "item1",
+                    "item2",
+                    "item3"
+                ]
             },
             "Dinner": {
                 "items": []
+            }
+        }
+    },
+    {
+        "2022/06/15": {
+            "Breakfast": {
+                "items": []
+            },
+            "Lunch": {
+                "items": []
+            },
+            "Dinner": {
+                "items": [
+                    "item1",
+                    "item2",
+                    "item3"
+                ]
             }
         }
     }
@@ -65,4 +67,6 @@ order.forEach(orderObj => {
     }
 })
 
-console.log(order)
+// console.log(order)
+console.log(util.inspect(order, false, null, true /* enable colors */))
+// console.log(JSON.stringify(util.inspect(order, false, null, true /* enable colors */)))
