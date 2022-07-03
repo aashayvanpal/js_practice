6898ded59d3531f0b5bb6b512ad04daa13fcabbc
Aashay <arkhamknight.aashay@gmail.com>
2022-07-03 22:22:45 +0530

    Stack implementation

---
+++ datastructures/Stack.js
@@ -0,0 +1,21 @@
+// Stack class
+class Stack {
+
+    // Array is used to implement stack
+    constructor() {
+        this.items = [];
+    }
+
+    // Functions to be implemented
+    // push(item)
+    // push function
+    push(element) {
+        // push element into the items
+        this.items.push(element);
+    }
+    
+    // pop()
+    // peek()
+    // isEmpty()
+    // printStack()
+}
