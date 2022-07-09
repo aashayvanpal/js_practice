482f28d64d13af0b0ef646315b68bc6dda72bbc6
Aashay <arkhamknight.aashay@gmail.com>
2022-07-09 19:14:26 +0530

    Queue added

---
+++ datastructures/Queue.js
@@ -0,0 +1,19 @@
+// Queue class
+class Queue {
+    // Array is used to implement a Queue
+    constructor() {
+        this.items = [];
+    }
+
+    // Functions to be implemented
+    // enqueue(item)
+    // enqueue function
+    enqueue(element) {
+        // adding element to the queue
+        this.items.push(element);
+    }
+    // dequeue()
+    // front()
+    // isEmpty()
+    // printQueue()
+}
