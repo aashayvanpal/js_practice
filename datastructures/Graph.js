10a709a1ef71f450c2eb660105ec26a5e0b0f6af
Aashay <arkhamknight.aashay@gmail.com>
2022-07-16 22:15:33 +0530

    added addVertex function

--- datastructures/Graph.js
+++ datastructures/Graph.js
@@ -11,6 +11,13 @@ class Graph {
     // functions to be implemented
 
     // addVertex(v)
+    // add vertex to the graph
+    addVertex(v) {
+        // initialize the adjacent list with a
+        // null array
+        this.AdjList.set(v, []);
+    }
+    
     // addEdge(v, w)
     // printGraph()
 
