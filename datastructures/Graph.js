5f9cb5e4979e02c83550fadb4f5069121e19e3c2
Aashay <arkhamknight.aashay@gmail.com>
2022-07-17 18:29:43 +0530

    added addEdge function

--- datastructures/Graph.js
+++ datastructures/Graph.js
@@ -17,8 +17,18 @@ class Graph {
         // null array
         this.AdjList.set(v, []);
     }
-    
+
     // addEdge(v, w)
+    // add edge to the graph
+    addEdge(v, w) {
+        // get the list for vertex v and put the
+        // vertex w denoting edge between v and w
+        this.AdjList.get(v).push(w);
+
+        // Since graph is undirected,
+        // add an edge from w to v also
+        this.AdjList.get(w).push(v);
+    }
     // printGraph()
 
     // bfs(v)
