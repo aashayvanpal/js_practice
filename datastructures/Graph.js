eb2da668ab398d2c713729360a933c821cf342ab
Aashay <arkhamknight.aashay@gmail.com>
2022-07-18 22:43:33 +0530

    added printGraph function

--- datastructures/Graph.js
+++ datastructures/Graph.js
@@ -30,6 +30,27 @@ class Graph {
         this.AdjList.get(w).push(v);
     }
     // printGraph()
+    // Prints the vertex and adjacency list
+    printGraph() {
+        // get all the vertices
+        var get_keys = this.AdjList.keys();
+
+        // iterate over the vertices
+        for (var i of get_keys) {
+            // great the corresponding adjacency list
+            // for the vertex
+            var get_values = this.AdjList.get(i);
+            var conc = "";
+
+            // iterate over the adjacency list
+            // concatenate the values into a string
+            for (var j of get_values)
+                conc += j + " ";
+
+            // print the vertex and its adjacency list
+            console.log(i + " -> " + conc);
+        }
+    }
 
     // bfs(v)
     // dfs(v)
