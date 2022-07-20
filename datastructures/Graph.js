da35de94874def8c7d4ceec5543df3dce8cf4a18
Aashay <arkhamknight.aashay@gmail.com>
2022-07-20 17:35:40 +0530

    BFS function added

--- datastructures/Graph.js
+++ datastructures/Graph.js
@@ -53,7 +53,45 @@ class Graph {
     }
 
     // bfs(v)
+    // function to performs BFS
+    bfs(startingNode) {
+
+        // create a visited object
+        var visited = {};
+
+        // Create an object for queue
+        var q = new Queue();
+
+        // add the starting node to the queue
+        visited[startingNode] = true;
+        q.enqueue(startingNode);
+
+        // loop until queue is empty
+        while (!q.isEmpty()) {
+            // get the element from the queue
+            var getQueueElement = q.dequeue();
+
+            // passing the current vertex to callback function
+            console.log(getQueueElement);
+
+            // get the adjacent list for current vertex
+            var get_List = this.AdjList.get(getQueueElement);
+
+            // loop through the list and add the element to the
+            // queue if it is not processed yet
+            for (var i in get_List) {
+                var neigh = get_List[i];
+
+                if (!visited[neigh]) {
+                    visited[neigh] = true;
+                    q.enqueue(neigh);
+                }
+            }
+        }
+    }
+
     // dfs(v)
+
 }
 
 // Using the above implemented graph class
@@ -84,3 +122,4 @@ g.addEdge('C', 'F');
 // E -> A D F C
 // F -> E C
 g.printGraph();
+
