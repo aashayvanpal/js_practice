f02be225265f33966d0bf36d805608fba244fa8b
Aashay <arkhamknight.aashay@gmail.com>
2022-07-21 21:17:06 +0530

    added DFS function

--- datastructures/Graph.js
+++ datastructures/Graph.js
@@ -91,7 +91,28 @@ class Graph {
     }
 
     // dfs(v)
+    // Main DFS method
+    dfs(startingNode) {
 
+        var visited = {};
+
+        this.DFSUtil(startingNode, visited);
+    }
+
+    // Recursive function which process and explore
+    // all the adjacent vertex of the vertex with which it is called
+    DFSUtil(vert, visited) {
+        visited[vert] = true;
+        console.log(vert);
+
+        var get_neighbours = this.AdjList.get(vert);
+
+        for (var i in get_neighbours) {
+            var get_elem = get_neighbours[i];
+            if (!visited[get_elem])
+                this.DFSUtil(get_elem, visited);
+        }
+    }
 }
 
 // Using the above implemented graph class
@@ -122,4 +143,10 @@ g.addEdge('C', 'F');
 // E -> A D F C
 // F -> E C
 g.printGraph();
+
+// prints
+// BFS
+// A B D E C F
+console.log("BFS");
+g.bfs('A');
 
